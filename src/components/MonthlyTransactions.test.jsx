60bf968ade001e42f6ec7faa4dd9d0c4c39d8236
krishna pradhan <kpaccess@gmail.com>
2024-08-31 20:11:32 -0400

    added few more unit test

--- src/components/MonthlyTransactions.test.jsx
+++ src/components/MonthlyTransactions.test.jsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import MonthlyTransactions from "./MonthlyTransactions";
 import { transactions } from "../data/data";
+import { calculatePoints, totalPoint } from "../utils/calculatePoints";
+
+vi.mock("../utils/calculatePoints", () => ({
+  calculatePoints: vi.fn((amount) => amount),
+  totalPoint: vi.fn(() => 100),
+}));
 
 describe(" Monthly Transactions", () => {
   it("renders correctily with provided data", () => {
@@ -29,5 +35,10 @@ describe(" Monthly Transactions", () => {
 
     expect(screen.getByText("2024-06-02")).toBeInTheDocument();
     expect(screen.getByText("$ 200.65")).toBeInTheDocument();
+
+    expect(calculatePoints).toHaveBeenCalledWith(120.9);
+    expect(calculatePoints).toHaveBeenCalledWith(75.5);
+    expect(calculatePoints).toHaveBeenCalledWith(200.4);
+    expect(totalPoint).toHaveBeenCalledWith(transactions[0]);
   });
 });
--- src/components/Rewards.test.jsx
+++ src/components/Rewards.test.jsx
@@ -1,5 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Rewards from "./Rewards";
+import { fetchCustomerData } from "../api/api";
+import { transactions } from "../data/data";
+
+vi.mock("../api/api", () => ({
+  fetchCustomerData: vi.fn(),
+}));
 
 describe("Rewards", () => {
   it(" Renders loading state initially", () => {
@@ -12,5 +18,15 @@ describe("Rewards", () => {
     expect(
       screen.getByRole("columnheader", { name: /Total Points/i })
     ).toBeInTheDocument();
+  });
+
+  it(" render customer data after successful API call", async () => {
+    fetchCustomerData.mockResolvedValue([transactions[0]]);
+
+    render(<Rewards data={[transactions[0]]} />);
+
+    await waitFor(() => {
+      expect(screen.getByText("John Doe")).toBeInTheDocument();
+    });
   });
 });
