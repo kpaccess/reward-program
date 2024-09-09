8dd30f2e4d0553c352d426a877e2eaadf46fa225
krishna pradhan <kpaccess@gmail.com>
2024-09-09 12:22:48 -0400

    updated to check for year and months length

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,9 @@ export const fetchTransactions = async () => {
     data.forEach((customer) => {
       customer.transactions.forEach((transaction) => {
         const date = new Date(transaction.date);
-        months.add(date.getMonth() + 1);
+        if (date.getFullYear() === 2024) {
+          months.add(date.getMonth() + 1);
+        }
       });
     });
     return [...months].sort((a, b) => a - b);
@@ -21,21 +23,36 @@ export const fetchTransactions = async () => {
 
   // find three consecutive months from the sorted months
   const findConsecutiveMonths = (months) => {
-    for (let i = 0; i < months.length - 3; i++) {
+    //it iterates through the array of months and stops two elements before the end
+    // as we looking for 3 consecutive months
+
+    for (let i = 0; i < months.length - 2; i++) {
+      // months[i + 1] === months[i] checks if the next month is exactly one more then the current month
+      // months[i + 2] === months[i] + 2 checks if the month after that is exactly two more then the current month
       if (months[i + 1] === months[i] + 1 && months[i + 2] === months[i] + 2) {
         return [months[i], months[i + 1], months[i + 2]];
       }
     }
+
     return [];
   };
 
-  const filterTransactionByConsecutiveMonths = (data, consecutiveMonths) => {
+  const filterTransactionByConsecutiveMonths = (
+    data,
+    consecutiveMonths,
+    year
+  ) => {
     return data.map((customer) => {
       const filteredTransaction = customer.transactions.filter(
         (transaction) => {
           const transactionDate = new Date(transaction.date);
           const transactionMonth = transactionDate.getMonth() + 1;
-          return consecutiveMonths.includes(transactionMonth);
+
+          const transactionYear = transactionDate.getFullYear();
+          return (
+            consecutiveMonths.includes(transactionMonth) &&
+            year === transactionYear
+          );
         }
       );
 
@@ -52,7 +69,8 @@ export const fetchTransactions = async () => {
 
   const filteredData = filterTransactionByConsecutiveMonths(
     data,
-    consecutiveMonths
+    consecutiveMonths,
+    2024
   );
 
   return filteredData;
